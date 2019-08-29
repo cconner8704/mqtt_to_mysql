9a33f73769be8ceb70910236290bd86df185b81a
cconner <cconner@cloudera.com>
2019-08-29 16:17:21 -0400

    First commit

---
+++ Dockerfile
@@ -0,0 +1,31 @@
+FROM python:3
+MAINTAINER Chris Conner chrism.conner@gmail.com
+
+#Install requests, flask and supervisor
+RUN pip install requests
+RUN pip install mysql-python
+RUN pip install paho-mqtt
+RUN apt-get update && apt-get install -y supervisor
+
+#Make log dir for supervisor
+RUN mkdir -p /var/log/supervisor
+
+#Copy supervisor conf and app
+COPY mqtt-to-mysql-supervisord.conf /etc/supervisor/conf.d/mqtt-to-mysql-supervisord.conf
+COPY app.py /app.py
+
+#Environment variables
+ENV MQTTPORT 1883
+ENV MQTTHOST localhost
+ENV MQTTUSER user
+ENV MQTTPASS pass
+ENV MQTTTOPIC testtopic
+ENV MQTTCAFILE None
+ENV DBHOST localhost
+ENV DBPORT 3306
+ENV DBUSER user
+ENV DBPASS pass
+
+#Expose ports
+
+CMD ["/usr/bin/supervisord"]
---
+++ app.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python
+import json
+import argparse
+import sys
+import logging
+import MySQLdb
+import paho.mqtt.client as mqttClient
+import time
+import ssl
+
+class Database:
+    def __init__(self, host="localhost", user="root", password=None, db=None):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.db = db
+        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        self.cursor = self.connection.cursor()
+
+    def insert(self, query):
+        try:
+            logging.info("%s" % query)
+            self.cursor.execute(query)
+            self.connection.commit()
+        except Exception as e:
+            logging.exception("Insert Failed: %s" % e)
+            self.connection.rollback()
+
+    def query(self, query):
+        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
+        cursor.execute(query)
+
+        return cursor.fetchall()
+
+    def __del__(self):
+        self.connection.close()
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        logging.info("Connected to broker")
+        global Connected 
+        Connected = True
+    else:
+        logging.exception("Broker connection failed")
+
+def on_message(client, userdata, message):
+   global db
+   query = """ %s """ % message.payload.decode()
+   logging.debug("Message Recieved running insert: %s" % query)
+   db.insert(query)
+
+Connected = False
+
+parser = argparse.ArgumentParser()
+
+parser.add_argument('-P', '--mqttport', action='store', dest='mqttport',
+                    default=1883, help='Port for MQTT')
+
+parser.add_argument('-H', '--mqtthost', action='store', dest='mqtthost',
+                    default="localhost", help='Host for MQTT')
+
+parser.add_argument('-U', '--mqttuser', action='store', dest='mqttuser',
+                    default="user", help='User for MQTT')
+
+parser.add_argument('-W', '--mqttpass', action='store', dest='mqttpass',
+                    default="pass", help='Password for MQTT')
+
+parser.add_argument('-T', '--mqtttopic', action='store', dest='mqtttopic',
+                    default="testtopic", help='Subscription Topic for MQTT')
+
+parser.add_argument('-c', '--mqttcafile', action='store', dest='mqttcafile',
+                    default=None, help='CA File for TLS for MQTT')
+
+parser.add_argument('-d', '--dbhost', action='store', dest='dbhost',
+                    default="localhost", help='Host for DB')
+
+parser.add_argument('-p', '--dbport', action='store', dest='dbport',
+                    default=3306, help='Port for DB')
+
+parser.add_argument('-u', '--dbuser', action='store', dest='dbuser',
+                    default="user", help='User for DB')
+
+parser.add_argument('-w', '--dbpass', action='store', dest='dbpass',
+                    default="password", help='Password for DB')
+
+parser.add_argument('-v', '--verbose', action="store_true", dest='verbose',
+                    default=False, help='Enable debug logging')
+
+args = parser.parse_args()
+
+if args.verbose:
+    logging_level=logging.DEBUG
+    app_debug=True
+else:
+    logging_level=logging.INFO
+    app_debug=False
+
+logging.basicConfig(stream=sys.stdout, level=logging_level)
+
+try:
+  db = Database(host = args.dbhost, user = args.dbuser, password = args.dbpass, db = "mysql")
+except Exception as e:
+  logging.exception("Failed to connect to database: %s" % e)
+  sys.exit(1)
+
+
+broker_address = args.mqtthost
+port = args.mqttport
+user = args.mqttuser
+password = args.mqttpass
+topic = args.mqtttopic
+cafile = args.mqttcafile
+
+client = mqttClient.Client("PythonMysqlApp")
+client.username_pw_set(user, password=password)
+client.on_connect = on_connect
+client.on_message = on_message
+if cafile is not None and cafile != "None":
+  client.tls_set(cafile, tls_version=ssl.PROTOCOL_TLSv1_2)
+  client.tls_insecure_set(True)
+
+client.connect(broker_address, port=port)
+
+client.subscribe(topic, qos=1)
+try:
+  client.loop_forever()
+except KeyboardInterrupt:
+  client.disconnect()
+  sys.exit(1)
+
