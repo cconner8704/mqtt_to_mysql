f62ceee022823b4858ebbab7314298217c9cf6cd
cconner <cconner@cloudera.com>
2019-09-18 13:18:03 -0400

    Tried to include reconnect

--- app.py
+++ app.py
@@ -14,23 +14,70 @@ class Database:
         self.user = user
         self.password = password
         self.db = db
-        self.connection = MySQLdb.connect(self.host, self.user, self.password, self.db)
-        self.cursor = self.connection.cursor()
+        self.conn = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        try:
+            if (self.conn):
+                status = "DB init success"
+            else:
+                status = "DB init failed"
+            self.conn.autocommit(True)
+            self.cursor = self.conn.cursor()
+            logging.info("%s" % status)
+        except Exception as e:
+            status = "DB init fail %s " % str(e)
+            logging.info("%s" % status)
+
+    def reconnect(self)
+        self.conn = MySQLdb.connect(self.host, self.user, self.password, self.db)
+        try:
+            if (self.conn):
+                status = "DB reconnect success"
+            else:
+                status = "DB reconnect failed"
+            self.conn.autocommit(True)
+            self.cursor = self.conn.cursor()
+            logging.info("%s" % status)
+        except Exception as e:
+            status = "DB reconnect fail %s " % str(e)
+            logging.info("%s" % status)
+
 
     def insert(self, query):
         try:
+            if self.conn is None:
+                self.__init__()
+            else:
+                self.conn.ping(True)
+                logging.info("%s" % query)
+                self.cursor.execute(query)
+        except MySQLdb.OperationalError:
+            self.reconnect()
             logging.info("%s" % query)
             self.cursor.execute(query)
-            self.connection.commit()
         except Exception as e:
             logging.exception("Insert Failed: %s" % e)
-            self.connection.rollback()
+            self.conn.rollback()
+
 
     def query(self, query):
-        cursor = self.connection.cursor( MySQLdb.cursors.DictCursor )
-        cursor.execute(query)
+        try:
+            if self.conn is None:
+                self.__init__()
+            else:
+                self.conn.ping(True)
+                logging.info("%s" % query)
+                self.cursor.execute(query)
+        except MySQLdb.OperationalError:
+            self.reconnect()
+            logging.info("%s" % query)
+            self.cursor.execute(query)
+        except Exception as e:
+            logging.exception("Query Failed: %s" % e)
+            self.conn.rollback()
+            return False
 
-        return cursor.fetchall()
+        return self.cursor.fetchall()
+
 
     def __del__(self):
         self.connection.close()
